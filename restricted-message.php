04f601e8119b849879e3258d702a601738dfef43
Patrice <patrice@desaintsteban.fr>
2024-02-22 00:58:01 +0100

    Mise à jour de restricted-message.php et style.css

--- restricted-message.php
+++ restricted-message.php
@@ -1,2 +1,2 @@
 <?php
-echo \WC_Memberships_User_Messages::get_message_html('content_restricted');
+echo \WC_Memberships_User_Messages::get_message_html('content_restricted', array('post_id' => get_the_id(), 'products' => array( 133, 314, 214, 90 )));
